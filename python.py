4f6094931eee5a77a7fd3980d16d7cab50a48b74
Abhisalvi452 <122198016+Abhisalvi452@users.noreply.github.com>
2023-01-18 10:20:38 +0530

    python.py

---
+++ python.py
@@ -0,0 +1,16 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text("Hello! I am a Telegram bot.")
+
+def main():
+    # Replace YOUR_TOKEN with the token of your bot
+    updater = Updater("YOUR_TOKEN", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
